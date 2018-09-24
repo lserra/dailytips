a5fcad36ce7d1604175046d72a094222dc8be127
lserra <laercio.serra@gmail.com>
2018-09-24 09:00:32 -0300

    preparing to deploy

---
+++ sql/hashtags_201809232334.sql
@@ -0,0 +1,10 @@
+﻿INSERT INTO public.hashtags (id,"name",count) VALUES 
+(1,'data ingestion',1)
+,(3,'data analysis',1)
+,(2,'data integration',2)
+,(4,'data visualization',3)
+,(5,'data science',4)
+,(6,'machine learning',4)
+,(7,'big data',3)
+,(8,'business intelligence',1)
+;
---
+++ sql/tips_201809232335.sql
@@ -0,0 +1,23 @@
+﻿INSERT INTO public.tips (id,tweetid,"text",created,likes,retweets) VALUES 
+(244,'950018619794247680','If you are wondering about Data Science or Big Data. Here is the answer: https://t.co/nleGczhYce','2018-01-07 14:57:44.000',1,1)
+,(245,'922500858054631424','Data Science and Machine Learning: Sample 2 - Recommender System
+https://t.co/irPkHZVjP4','2017-10-23 16:31:58.000',3,2)
+,(246,'908712503563010048','How Important Is Advanced and Predictive Analytics for BI Professionals?  You want to know the answer, then read thi…https://t.co/ugDltMvCJz','2017-09-15 15:21:58.000',0,0)
+,(247,'908711561430052864','Machine Learning and BI what they have in common? Well, here is the answer: https://t.co/ugDltMvCJz','2017-09-15 15:18:13.000',0,0)
+,(248,'898871818634842113','Rio2016 - One Year later, simply amazing and unforgettable. https://t.co/BC2YKfxEmL','2017-08-19 11:38:36.000',0,0)
+,(249,'891278110888427520','This is a great example of how to democratize the data in your Company and to apply the self-service data culture - https://t.co/CCl9uNvU7B','2017-07-29 12:43:55.000',0,0)
+,(250,'885187843831848961','DATA VISUALIZATION OR INFOGRAPH? You want an answer for this question, then read this article: https://t.co/KxkIK3CsGo','2017-07-12 17:23:22.000',0,0)
+,(251,'883899542944481281','Do you want to know more about Data Science and Machine Learning then visit my blog and read the recent article. https://t.co/OsPzxLYlYm','2017-07-09 04:04:07.000',0,0)
+,(252,'876643739682447360','I''m too busy working on my own grass to notice if yours is greener!','2017-06-19 03:32:09.000',0,0)
+,(253,'875393551680626689','Já que o assunto é Data Science, no meu último post em https://t.co/RRoyefz0mw, eu faço uma pequena demo.','2017-06-15 16:44:21.000',0,0)
+;
+INSERT INTO public.tips (id,tweetid,"text",created,likes,retweets) VALUES 
+(254,'875385018247512064','Mas, encontrei em um outro blog que responde estas e outras perguntas de forma sucinta e clara: https://t.co/iMdWBRIpZP','2017-06-15 16:10:26.000',0,0)
+,(255,'875384711656419329','Estava pensando escrever e publicar um post (artigo) no meu blog: https://t.co/wvKADpSnGC, somente para esclarecer esta dúvidas.','2017-06-15 16:09:13.000',0,0)
+,(256,'875384103411089409','Sempre me perguntam: Como montar uma equipe de dados? Existe somente um tipo (perfil) de Data Scientist?','2017-06-15 16:06:48.000',0,0)
+,(257,'821382232753471489','Scala Data Analysis Cookbook via @a_ebook https://t.co/xRoKJX55ZI','2017-01-17 15:42:39.000',0,0)
+,(258,'781105903793758208','We did it! https://t.co/rskbRv6wMa','2016-09-28 12:18:54.000',0,0)
+,(259,'773527250482372608','A project that will be forever in my mind and my heart. Thanks Rio2016! https://t.co/a8Up8G4C3s','2016-09-07 14:24:02.000',0,0)
+,(260,'750060091211128832','Python + Pentaho Simplify Data Prep for Data Science/Machine Learning: https://t.co/OvYIktcmtg','2016-07-04 20:13:55.000',0,0)
+,(261,'750030359891472384','Data Science - o que é, pra que serve e como funciona? Quer saber, então participe do "Dojô de Data Science". https://t.co/C8GM5xKPuL','2016-07-04 18:15:47.000',0,0)
+;
