85500f3e833b26d88a1f42fe7d90b4fa5be66a2d
lserra <laercio.serra@gmail.com>
2018-11-28 23:35:50 -0200

    fixing bugs with pycodestyle

--- tasks_etl.py
+++ tasks_etl.py
@@ -3,11 +3,13 @@
 
 
 """
-This task should be run in a daily cronjob to look for new tips and update stats (number of likes and retweets)
-on existing tweets. The tables are recreated daily.
+This task should be run in a daily cronjob to look for new tips and update
+stats (number of likes and retweets) on existing tweets.
+The tables are recreated daily.
 """
 from tasks.import_tweets import get_hashtag, get_tweet
-from tips.db import truncate_tables, get_hashtags, add_hashtags, get_tips, add_tips
+from tips.db import truncate_tables, get_hashtags, add_hashtags, get_tips,
+add_tips
 
 
 if __name__ == '__main__':
