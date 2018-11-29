12cbc595f7294d48d2c889fc5bb5bb2a3723d3e5
lserra <laercio.serra@gmail.com>
2018-11-28 23:19:46 -0200

    fixing bugs with pycodestyle

--- tasks/import_tweets.py
+++ tasks/import_tweets.py
@@ -3,16 +3,17 @@
 
 
 """
-This task should be run in a daily cronjob to look for new tips and update stats (number of likes and retweets)
-on existing tweets. The tables are recreated daily.
+This task should be run in a daily cronjob to look for new tips and update
+stats (number of likes and retweets) on existing tweets.
+The tables are recreated daily.
 """
 import os
 import re
 import sys
 import tweepy
-from tips.db import truncate_tables, get_hashtags, add_hashtags, get_tips, add_tips
-
 from collections import Counter
+from tips.db import truncate_tables, get_hashtags, add_hashtags, get_tips,
+add_tips
 
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
@@ -23,7 +24,8 @@ CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
 
-TWITTER_ACCOUNT = os.environ.get('BIGDATA_TIP_APP_TWITTER_ACCOUNT') or 'datafresh'
+TWITTER_ACCOUNT = os.environ.get('BIGDATA_TIP_APP_TWITTER_ACCOUNT') or
+'datafresh'
 EXCLUDE_PYTHON_HASHTAG = True
 TAG = re.compile(r'#([a-z0-9]{3,})')    # Extracting hashtags from the tips
 
@@ -36,8 +38,8 @@ def _get_twitter_api_session():
 
 def _get_tweets(screen_name=TWITTER_ACCOUNT):
     """
-    The exclude_replies=True and include_rts=False arguments are convenient because we only want Daily Big Data Tip’s
-    own tweets (not re-tweets).
+    The exclude_replies=True and include_rts=False arguments are convenient
+    because we only want Daily Big Data Tip’s own tweets (not re-tweets).
     """
     api = _get_twitter_api_session()
     return tweepy.Cursor(api.user_timeline,
@@ -48,8 +50,9 @@ def _get_tweets(screen_name=TWITTER_ACCOUNT):
 
 def get_hashtag_counter(tips):
     """
-    The collections.Counter returns a dict like object with the tags as keys, and counts as values, ordered in
-    descending order by values (most common). I excluded the too common python tag which would skew the results.
+    The collections.Counter returns a dict like object with the tags as keys,
+    and counts as values, ordered in descending order by values (most common).
+    I excluded the too common python tag which would skew the results.
     """
     blob = ' '.join(t.text.lower() for t in tips)
     cnt = Counter(TAG.findall(blob))    # to get all tags
